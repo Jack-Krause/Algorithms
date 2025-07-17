d22212521acb3305b51beaca5025b00279b528c0
Jack Krause <jmkrause@iastate.edu>
2025-07-17 14:38:00 -0500

    MergeSort file added

---
+++ code/algorithms/java_proj/src/sorting/MergeSort.java
@@ -0,0 +1,18 @@
+package sorting;
+
+import core.Algorithm;
+
+import java.util.List;
+
+public class MergeSort<T extends Comparable<? super T>>
+    implements Algorithm<List<T>, List<T>> {
+    /**
+     * Performs the MergeSort algorithm on a list of type T
+     * @param input List to sort
+     * @return sorted list of type T
+     */
+    @Override
+    public List<T> run(List<T> input) {
+        return List.of();
+    }
+}
