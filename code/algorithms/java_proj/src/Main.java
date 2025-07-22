c3e9cbf5548d490eaebefc83609f35019735d581
Jack Krause <jmkrause@iastate.edu>
2025-07-22 11:40:57 -0500

    update

--- code/algorithms/java_proj/src/Main.java
+++ code/algorithms/java_proj/src/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         double alpha = 0.85;
         int[][] testCases = {
-                {}, {5,5,5,5,5,5,5,5,5,5},
+                {},
+                {5,5,5,5,5,5,5,5,5,5},
                 {1,2,3,4,5,6,7,8,9,10},
                 {10,9,8,7,6,5,4,3,2,1},
                 {3,7,1,9,2,8,6,4,5,0},
